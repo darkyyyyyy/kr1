32ade8984337ccd1d22bc7acb63c049920798bd8
darkyyyyyy <134320317+darkyyyyyy@users.noreply.github.com>
2023-05-22 21:08:15 +0300

    Update env.lua

--- env.lua
+++ env.lua
@@ -4,7 +4,20 @@
 local env = getfenv(2)
 local utilities = {}
 
+local loggers = {}
+loggers.__index = loggers
+
 --------// Functions
+
+function utilities.toStringTable(table: {any: any}, indexToo)
+	local result = {}
+	
+	for index, value in pairs(table) do
+		result[indexToo and tostring(index) or index] = tostring(value)
+	end
+	
+	return result
+end
 
 function utilities.new(class: string)
 	assert(class, `Class required.`)
@@ -18,6 +31,38 @@ function utilities.new(class: string)
 		return instance
 	end
 end
+
+function utilities.messageLogger(logger)
+	env.print = function(...)
+		return logger:add(...)
+	end
+	env.warn = function(...)
+		return logger:add(...)
+	end
+end
+
+---// Loggers
+
+function utilities.newLogger()
+	local logger = setmetatable({}, loggers)
+	loggers.logs = {}
+	
+	return logger
+end
+
+function loggers:add(...)
+	assert(self, `logger:add must be called with : instead of .`)
+	
+	table.insert(self.logs, table.concat(utilities.toStringTable(...), " "))
+	return true
+end
+
+function loggers:get()
+	assert(self, `logger:get must be called with : instead of .`)
+	
+	return self.logs
+end
+
 
 --------// Tables
 
